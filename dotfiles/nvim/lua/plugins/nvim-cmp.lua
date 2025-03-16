1b5a7e0798b287520fbe8ef1ea7c6f77add2813b
ItzEmoji <cyril.dettling@gmail.com>
2025-03-16 10:28:13 +0100

    fixed nvim errors

--- dotfiles/nvim/lua/plugins/nvim-cmp.lua
+++ dotfiles/nvim/lua/plugins/nvim-cmp.lua
@@ -17,13 +17,13 @@ return {
   },
   config = function()
     local cmp = require("cmp")
-
     local luasnip = require("luasnip")
-
     local lspkind = require("lspkind")
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
+
+    -- Configure auto-pairs
 
     cmp.setup({
       completion = {
@@ -45,7 +45,7 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
-        { name = "nvim_lsp"},
+        { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
@@ -61,3 +61,4 @@ return {
     })
   end,
 }
+
